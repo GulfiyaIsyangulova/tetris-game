da82ae777e5dcb931f266701d70e198dbf969275
Gulfiya Isyangulova <gulfiyaisyangulova@Gulfiyas-MacBook-Pro.local>
2019-06-25 22:30:45 -0400

    nothing works

--- main.js
+++ main.js
@@ -5,28 +5,88 @@ let Square;
 let visibleShapes = [];
 
 const reachedEndEvent = new Event("reachedEnd");
+const theRowIsFilledEvent = new Event("theRowIsfilled");
 
 window.onload = function () {
     canvas = document.getElementById('game-board');
     ctx = canvas.getContext('2d');
     canvas.addEventListener('reachedEnd', createShapeRand);
+    canvas.addEventListener('theRowIsFilled', countFilledRows)
 
     document.getElementById("start-game-button").onclick = function () {
         startGame();
     };
-    
 }
 
 function startGame() {
     createShapeRand();
     animate();
 }
+function countFilledRows() {
+    // if (visibleShapes[i].length > 50  && visibleShapes[i].width >50 ) {}
+
+    // let i = visibleShapes.x;
+    // let j = visibleShapes.y;
+
+    //     var c = 0;
+    // for( var i in arr )
+    //   for( var j in arr[ i ] )
+    //     if( arr[ i ][ j ] != undefined )
+    //       ++c;
+
+    let rows = {};
+    let successRow;
+
+    for (var i in visibleShapes) {
+
+        let baseline = (visibleShapes[i].y + visibleShapes[i].height);
+
+        if (rows[baseline]) {
+            rows[baseline] += 1;
+
+        } else {
+            rows[baseline] = 1;
+        }
+    }
+
+    for (let key in rows) {
+        if (rows[key] === 10) {
+
+            successRow = key;
 
 
+        }
+    }
+
+    if (successRow) {
+
+        ctx.clearRect(0, successRow - 50, 500, 50)
+
+    }
+
+    // return rows;
+
+
+    // for (var i = 0; i < visibleShapes.length; i++) { 
+    //     for (var j = 0; j < visibleShapes.length; j++) { 
+
+    //         rows[i][j] = s[h++]; 
+    //     } 
+    // } 
+
+
+
+    // for (var i=0; i<visibleShapes.length; i++){
+    //     if ((oneShape.visibleShapes[index]).x > (oneShape.visibleShapes[index-1]).x){
+    //         rows.push(oneshape.visibleShapes.y);
+    //     }
+    // }
+    // console.log("clear row`")
+}
 
 function createShapeRand() {
     console.log('testing', visibleShapes);
-
+    //makeEverythingASquare()
     Rectangle = new Shapes(250, 0, 50, 100);
     Square = new Shapes(250, 0, 50, 50)
     allTheShapes = [Rectangle, Square];
@@ -34,9 +94,6 @@ function createShapeRand() {
     const randomShapeIndex = Math.floor(Math.random() * allTheShapes.length);
     const shape = allTheShapes[randomShapeIndex];
 
-    // visibleShapes.push(shape)
-    
-    // shape.drawItself();
     shape.moveDownForever();
     document.onkeydown = function (e) {
         let directions = ["ArrowLeft", "ArrowRight", "ArrowDown"];
@@ -44,47 +101,30 @@ function createShapeRand() {
             shape.moveYourself(e.key);
         }
     }
-
-    // shape.moveYourself();
 }
 
-
-function drawAllShapes(){
-    visibleShapes.forEach((oneShape)=>{
+function drawAllShapes() {
+    visibleShapes.forEach((oneShape) => {
         // console.log('---', i, visibleShapes)
+
         oneShape.drawItself();
-    })
+    });
 }
 
-function animate(){
+function animate() {
+    const interval = setInterval(() => {
 
-    
-    setInterval(()=>{
         ctx.clearRect(0, 0, 500, 650);
         drawAllShapes();
-    }, 40)
+        // countFilledRows()
+        makeEverythingASquare()
 
+        // console.log(visibleShapes)
+
+        if (visibleShapes.length > 20) {
+            alert("You lose!")
+            location.reload();
+            clearInterval(interval);
+        }
+    }, 40);
 }
-
-
-// function randomShape() {
-    //randomly choose a shape
-
-
-
-
-
-
-
-    //when shape reaches end
-
-    // setInterval(() =>{
-    //     do { moveDownForever(randomShape) }
-    //     while(!randomShape.reachedEnd == true);
-
-    // },20)
-
-
-    //call function again (recursion)
-
-// randomShapeRec();
